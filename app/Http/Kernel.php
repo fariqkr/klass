46eb70f2e36eb1f76533664995f4049c3d568d09
Dani Ihza Farrosi <dani.videda@gmail.com>
2020-12-01 13:09:35 +0700

    add middleware to access Student and Teacher dashboard. Login and register also redirected to corresponding dashboard if already logged in

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -54,10 +54,13 @@ class Kernel extends HttpKernel
      */
     protected $routeMiddleware = [
         'auth' => \App\Http\Middleware\Authenticate::class,
+        'auth.student' => \App\Http\Middleware\Student::class,
+        'auth.teacher' => \App\Http\Middleware\Teacher::class,
         'auth.basic' => \Illuminate\Auth\Middleware\AuthenticateWithBasicAuth::class,
         'cache.headers' => \Illuminate\Http\Middleware\SetCacheHeaders::class,
         'can' => \Illuminate\Auth\Middleware\Authorize::class,
         'guest' => \App\Http\Middleware\RedirectIfAuthenticated::class,
+        'guest.custom' => \App\Http\Middleware\RedirectIfAuthenticatedAsStudentOrTeacher::class,
         'password.confirm' => \Illuminate\Auth\Middleware\RequirePassword::class,
         'signed' => \Illuminate\Routing\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
--- routes/web.php
+++ routes/web.php
@@ -19,64 +19,65 @@ use Illuminate\Support\Facades\Route;
 */
 Route::get('/', function () {
     return view('home');
-})->name('home');
+})->name('home')->middleware('guest');
 
-Route::get('/student/register', [RegisterController::class, 'showStudentRegisterForm'])->name('register.student');
-Route::post('/student/register', [RegisterController::class, 'registerStudent']);
-
-Route::get('/teacher/register', [RegisterController::class, 'showTeacherRegisterForm'])->name('register.teacher');
-Route::post('/teacher/register', [RegisterController::class, 'registerTeacher']);
-
-Route::get('/student/login', [LoginController::class, 'showStudentLoginForm'])->name('login.student');
-Route::post('/student/login', [LoginController::class, 'loginStudent'])->name('login.student');
-
-Route::get('/teacher/login', [LoginController::class, 'showTeacherLoginForm'])->name('login.teacher');
-Route::post('/teacher/login', [LoginController::class, 'loginTeacher'])->name('login.teacher');
-
-Route::post('student/logout', [LogoutController::class, 'logoutStudent'])->name('logout.student');
-Route::post('teacher/logout', [LogoutController::class, 'logoutTeacher'])->name('logout.teacher');
-
-Route::get('/student/dashboard', [StudentController::class, 'index'])->name('student.dashboard');
-Route::get('/teacher/dashboard', [TeacherController::class, 'index'])->name('teacher.dashboard');
-
-Route::get('/student/join', function () {
-    return view('student.home.joinclass');
-});
-Route::get('/teacher/createclass', function () {
-    return view('teacher.home.createclass');
+Route::middleware('auth.student')->group(function () {
+    Route::get('/student/dashboard', [StudentController::class, 'index'])->name('student.dashboard');
+    Route::get('/student/join', function () {
+        return view('student.home.joinclass');
+    });
+    Route::get('/student/subjectmatter', function () {
+        return view('student.course.subjectmatter');
+    });
+    Route::get('/student/assignment', function () {
+        return view('student.course.assignment');
+    });
+    Route::get('/student/review', function () {
+        return view('student.course.review');
+    });
 });
 
-Route::get('/teacher/createsubjectmatter', function () {
-    return view('teacher.class.createsubjectmatter');
+Route::middleware('auth.teacher')->group(function () {
+    Route::get('/teacher/dashboard', [TeacherController::class, 'index'])->name('teacher.dashboard');
+    Route::get('/teacher/createclass', function () {
+        return view('teacher.home.createclass');
+    });
+    Route::get('/teacher/createsubjectmatter', function () {
+        return view('teacher.class.createsubjectmatter');
+    });
+    Route::get('/teacher/createassignment', function () {
+        return view('teacher.class.createassignment');
+    });
+    Route::get('/teacher/createquiztest', function () {
+        return view('teacher.class.createquiztest');
+    });
+    Route::get('/teacher/createtask', function () {
+        return view('teacher.class.createtask');
+    });
+    Route::get('/teacher/subjectmatter', function () {
+        return view('teacher.course.subjectmatter');
+    });
+    Route::get('/teacher/assignment', function () {
+        return view('teacher.course.assignment');
+    });
+    Route::get('/teacher/review', function () {
+        return view('teacher.course.review');
+    });
 });
 
-Route::get('/teacher/createassignment', function () {
-    return view('teacher.class.createassignment');
-});
-Route::get('/teacher/createquiztest', function () {
-    return view('teacher.class.createquiztest');
-});
-Route::get('/teacher/createtask', function () {
-    return view('teacher.class.createtask');
-});
+Route::middleware('guest.custom')->group(function () {
+    Route::get('/student/register', [RegisterController::class, 'showStudentRegisterForm'])->name('register.student');
+    Route::post('/student/register', [RegisterController::class, 'registerStudent']);
 
-Route::get('/student/subjectmatter', function () {
-    return view('student.course.subjectmatter');
-});
-Route::get('/teacher/subjectmatter', function () {
-    return view('teacher.course.subjectmatter');
-});
+    Route::get('/teacher/register', [RegisterController::class, 'showTeacherRegisterForm'])->name('register.teacher');
+    Route::post('/teacher/register', [RegisterController::class, 'registerTeacher']);
 
-Route::get('/student/assignment', function () {
-    return view('student.course.assignment');
-});
-Route::get('/teacher/assignment', function () {
-    return view('teacher.course.assignment');
-});
+    Route::get('/student/login', [LoginController::class, 'showStudentLoginForm'])->name('login.student');
+    Route::post('/student/login', [LoginController::class, 'loginStudent'])->name('login.student');
 
-Route::get('/student/review', function () {
-    return view('student.course.review');
-});
-Route::get('/teacher/review', function () {
-    return view('teacher.course.review');
+    Route::get('/teacher/login', [LoginController::class, 'showTeacherLoginForm'])->name('login.teacher');
+    Route::post('/teacher/login', [LoginController::class, 'loginTeacher'])->name('login.teacher');
+
+    Route::post('/student/logout', [LogoutController::class, 'logoutStudent'])->name('logout.student');
+    Route::post('/teacher/logout', [LogoutController::class, 'logoutTeacher'])->name('logout.teacher');
 });
